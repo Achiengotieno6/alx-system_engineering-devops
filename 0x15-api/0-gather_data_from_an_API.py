401b8308652e7b95557f1729c59e84b380951af3
Achiengotieno6 <audrey.achieng@gmail.com>
2023-10-29 00:48:44 -0700

    API concept

---
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+"""returns information using REST API"""
+
+import requests
+import sys
+
+
+if __name__ == "__main__":
+     employee_id = sys.argv[1]
+
+     base_url = "https://jsonplaceholder.typicode.com"
+
+     url_employee = "{}/users/{}".format(base_url, employee_id)
+     response = requests.get(url_employee)
+     employee_data = response.json()
+
+     todo_url = "{}/users/{}/todos".format(base_url, employee_id)
+     response = requests.get(todo_url)
+
+     tasks = response.json()
+     completed_tasks = [task for task in tasks if task.get('completed')]
+
+     print("Employee {} is done with tasks({}/{}):"
+          .format(employee_data.get('name'), len(completed_tasks), len(tasks)))
+
+     for task in completed_tasks:
+        print("\t {}".format(task.get('title')))
